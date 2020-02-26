bfe35674db50c0a23115f96cf3ad38e473bd3b3a
Doug Sappet <dsappet@gmail.com>
2020-02-25 23:26:06 -0500

    outlining a few tests, currently no implementation

--- src/set-content.spec.ts
+++ src/set-content.spec.ts
@@ -7,15 +7,31 @@ import * as puppeteer from "puppeteer";
 //import { AddTagOptions } from './interfaces/add-tag-options';
 
 describe('e2eSetContent', () => {
-  test('it should pass', () => {
+  test('shall pass', () => {
     expect(true).toBe(true);
   })
-  test('it should return a puppeteer Response object', async () => {
+  test('shall return a puppeteer Response object', async () => {
     // let browser = await createJestPuppeteerEnvironment();
     // let b = new browser({});
     // let page = await b.newPuppeteerPage();
 
 
     // const result = e2eSetContent(page, '<tomato-soup></tomato-soup>');
+  });
+
+  test('shall inject script and style tags when supplied', () => {
+
+  });
+
+  test('shall accept and inject only script tags', () => {
+
+  });
+
+  test('shall accept and inject only style tags', () => [
+
+  ]);
+  
+  test('shall succeed when optional parameters are omitted', () => {
+
   });
 });
