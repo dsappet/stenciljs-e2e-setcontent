ecfe86a0140a739c3610a98a575287d37f07e9cf
Doug Sappet <dsappet@gmail.com>
2020-03-10 22:24:12 -0400

    chore: demo app working and test in demo app working

--- demo/src/components/my-component/my-component.e2e.ts
+++ demo/src/components/my-component/my-component.e2e.ts
@@ -1,32 +1,59 @@
-import { newE2EPage } from '@stencil/core/testing';
+import { newE2EPage, E2EPage } from '@stencil/core/testing';
+import { e2eSetContent, AddTagOptions} from 'stenciljs-e2e-setcontent';
+
+const includeScript = 'https://pastebin.com/raw/yr7PcnqD';
+
+interface myE2EPage extends E2EPage {
+  /**
+   * Instead of testing a url directly, html content can be mocked using
+   * `page.setContent(html)`. A shortcut to `page.setContent(html)` is to set
+   * the `html` option when creating a new page, such as
+   * `const page = await newE2EPage({ html })`.
+   */
+  //setContent(page:any, html: string, navigationOptions?: any, moreOptions?: any): Promise<void>;
+  betterSetContent: any;
+}
 
 describe('my-component', () => {
   it('renders', async () => {
     const page = await newE2EPage();
+
+    //page.setContent = e2eSetContent.bind(null, page);
 
     await page.setContent('<my-component></my-component>');
     const element = await page.find('my-component');
     expect(element).toHaveClass('hydrated');
   });
 
-  it('renders changes to the name data', async () => {
-    const page = await newE2EPage();
+  it('proves that addScriptTag() does not work', async () => {
+    let page = await newE2EPage();
 
+    await page.addScriptTag({url: includeScript});
     await page.setContent('<my-component></my-component>');
-    const component = await page.find('my-component');
-    const element = await page.find('my-component >>> div');
-    expect(element.textContent).toEqual(`Hello, World! I'm `);
+    // If you reverse the setContent and addScriptTag it will "work" but if the component requires the content of the script it is still not good enough
 
-    component.setProperty('first', 'James');
-    await page.waitForChanges();
-    expect(element.textContent).toEqual(`Hello, World! I'm James`);
+    let result = await page.evaluate(() => {
+      return window["myDemoVar"]
+    })
 
-    component.setProperty('last', 'Quincy');
-    await page.waitForChanges();
-    expect(element.textContent).toEqual(`Hello, World! I'm James Quincy`);
+    expect(result).toBeUndefined();
+  })
 
-    component.setProperty('middle', 'Earl');
-    await page.waitForChanges();
-    expect(element.textContent).toEqual(`Hello, World! I'm James Earl Quincy`);
+  it('loads the remote script', async () => {
+    let page = await newE2EPage() as myE2EPage;
+
+    page.betterSetContent = e2eSetContent; //.bind(null, page);
+
+    const addTags: AddTagOptions = {
+      script: [includeScript]
+    }
+
+    await page.betterSetContent(page,'<my-component></my-component>', {}, addTags);
+    let result = await page.evaluate(() => {
+      return window["myDemoVar"]
+    })
+
+    expect(result).toEqual('monkey tuna');
   });
+
 });
